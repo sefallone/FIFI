b7572ea6d2713566205f59d4724942647f3b413e
sefallone <166940959+sefallone@users.noreply.github.com>
2025-05-25 23:45:54 +0200

    Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -9,8 +9,8 @@ from datetime import datetime
 st.set_page_config(page_title="Dashboard FIFI", layout="wide")
 # Logo en barra lateral
 with st.sidebar:
-    logo = Image.open("logo.jpg")
-    st.image(logo, caption="Fallone Investments", use_column_width=True)
+    logo = Image.open("Logo.jpg")
+    st.image(logo, capton="Fallone Investments", use_column_width=True)
     st.title("Configuración")
     
 # Subida de archivo
