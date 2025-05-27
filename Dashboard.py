2a61d504c80cdf0d01940cb1e986e461c7d939e3
sefallone <166940959+sefallone@users.noreply.github.com>
2025-05-27 09:07:00 +0200

    Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -14,20 +14,17 @@ from io import BytesIO
 st.set_page_config(page_title="Dashboard FIFI", layout="wide")
 # Logo en página principal
 # Logo en página principal
-with open("Logo.jpg", "rb") as f:
-    logo_base64 = base64.b64encode(f.read()).decode()
-
-st.markdown(f"""
-    <div style='
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-bottom: 30px;'>
-
-        <img src='data:image/jpeg;base64,{logo_base64}' style='height: 100px; margin-right: 20px;'/>
-        <h2 style='margin: 0; font-size: 26px; color: #333;'>Fallone Investments</h2>
+logo = Image.open("Logo.jpg")
+st.markdown("""
+    <div style='text-align: center;'>
+        <img src='data:image/jpeg;base64,{}' style='width:200px;'/>
+        <h3 style='margin-top:10px;'>Fallone Investments</h3>
     </div>
-""", unsafe_allow_html=True)
+    """.format(base64.b64encode(open("Logo.jpg", "rb").read()).decode()), unsafe_allow_html=True)
+
+# Sidebar config
+with st.sidebar:
+    st.title("Configuración")
 # Sidebar config
 with st.sidebar:
     st.title("Configuración")
